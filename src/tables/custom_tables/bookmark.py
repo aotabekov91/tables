410cc767c46307e0afb88a33a74509d5aadc9be8
adam <adam@myworld.net>
2023-11-26 03:11:20 +0100

    cleaned up bookmakr

--- src/tables/custom_tables/bookmark.py
+++ src/tables/custom_tables/bookmark.py
@@ -4,16 +4,11 @@ class Bookmark(Table):
 
     name='bookmark' 
     dname='bookmark'
+    uniq={'uniq_b':  ('hash', 'kind', 'position')}
     fields = [
         'id integer PRIMARY KEY AUTOINCREMENT',
-        'url text',
         'hash text',
         'text text',
-        'title text',
-        'page integer',
-        'keyword text',
-        'summary text',
         'position text',
-        'kind text not null',
-        'constraint unique_doc_bookmark unique (hash, page, position)',
-        'constraint unique_web_bookmark unique (url)']
+        'kind text not null',]
+
